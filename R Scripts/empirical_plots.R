ffd690433f21ca9726673ff398e109e4dfa80c9d
Philipp Koncar <p.koncar@student.tugraz.at>
2015-03-30 16:41:48 +0200

    update of plot scripts

--- R Scripts/empirical_plots.R
+++ R Scripts/empirical_plots.R
@@ -1,79 +1,90 @@
-#__author__ = 'Simon Walk, Florian Geigl, Denis Helic'
-#__license__ = "GPL"
-#__version__ = "0.0.1"
-#__email__ = "simon.walk@tugraz.at"
-#__status__ = "Development"
-
 args <- commandArgs(trailingOnly=T)
 print(" ++ Setting work directory")
 setwd(args[1])
-print(" ++ Reading weights table")
-t = read.table(args[2], sep="\t", header=F, fill=T)
-t <- t(t)
-dtau = args[3]
-dpsi = args[4]
-ac = args[5]
-k1 = args[6]
-#intrinsic = read.table(args[8], sep="\t", header=F, fill=T)
-#extrinsic = read.table(args[9], sep="\t", header=F, fill=T)
-#intrinsic <- abs(intrinsic)
-print(" ++ Plotting data (ratios)")
-pdf(paste(args[7], "_ratio.pdf", sep=""))
-max_y_l = max(t[,2], na.rm=T)
-max_x_b = max(t[,1], na.rm=T)
-max_y_r = max(t[,4], na.rm=T)
-max_x_t = max(t[,3], na.rm=T)
-cex_size = 0.6
-
-par(mar=c(5,4,4,1)+.1)
-c1 <- rgb(0.5, 0.5, 0.5, alpha=1)
-c2 <- rgb(0,0,0,alpha=1)
-c3 <- rgb(0.8, 0.4, 0.3, alpha=0.9)
+print(" ++ Reading combined data")
+data = read.table(args[2], sep="\t", header=T)
+print(" ++ Reading taus data")
+sim_act_x = read.table(args[4], header=F)
+print(" ++ Reading weights data")
+sim_act_y = read.table(args[3], header=F)
+graph_name = paste(args[5], args[6], sep="_")
+mode = args[6]
+dtau = args[7]
+mu = args[8]
+ac = args[9]
+k1 = args[10]
 cex_paper = 1.5
+cex_size = 0.6
+colors = c("#000000", "#858585")
+xlabel = substitute(tau ~ " (in " * mode * ")", list(mode=mode))
+if (args[6] == "days") {
+  linetype = "l"
+  pchstyle = NA
+} else {
+  linetype = "l"
+  pchstyle = NA
+}
 
-plot(t[,1], t[,2], type="l", pch=1, xlab=expression(tau ~ " (in months)"), ylab="Ratio", 
-     col=c1, cex=cex_size, xlim=c(0, max_x_t), cex.axis=cex_paper, cex.lab=cex_paper)
-
-print(" ++ Plotting data (activity)")
-
-x = seq(0,max_x_t,1)
-y = as.numeric(rep(k1, length(x)))
-rho = sd(t[,2], na.rm=TRUE)
+print(" ++ Plotting ratios and k1")
+pdf(paste(graph_name, "_ratios.pdf", sep=""))
+min_y = min(data$ratios)
+max_y = max(data$ratios)
+print(min_y)
+print(max_y)
+k1_x = seq(1,length(data$ratios),1)
+k1_y = as.numeric(rep(k1, length(k1_x)))
+plot(data$ratios, type=linetype, col=colors[1], xlab=xlabel, ylab="Ratio", cex.axis=cex_paper, cex.lab=cex_paper, ylim=c(min_y, max_y), pch=1)
+lines(k1_x, k1_y, lty=2, col=colors[2])
+rho = sd(data$ratios, na.rm=TRUE)
 rho_n = rho/as.numeric(k1)
-#rob = var(t[,2], na.rm=TRUE)
-#rob = rob/as.numeric(k1)
-#act = sum(t[,6], na.rm=TRUE)
-#months = max(t[,5], na.rm=T)
-#lmact  = t[,6][months+1]
-#mean_act = act/months
-#act_inertia = 1/rho
-#act_mass = 1/rho * act
-#mean_momentum = act_mass * mean_act
-#curr_momentum = act_mass * lmact
-
 rob = round(rho_n, digits=4)
-title(substitute(atop("Ratio " ~ (frac(lambda,mu)) ~ " over " ~ tau ~ " (in months)",
+title(substitute(atop("Ratio " ~ (frac(lambda,mu)) ~ " over " ~ tau ~ " (in " * mode * ")",
                       Delta ~ tau == ~ dt ~ ", " ~ mu == ~ dp ~ ", " ~ kappa[1] == ~ k1 ~ ", " ~ rho == ~ rho_n),
-                 list(dt = dtau, dp = dpsi, k1 = k1, rho_n=rob)), cex.main=cex_paper)
-lines(x, y, lty=2, col=c2)
+                 list(dt = dtau, dp = mu, k1 = k1, rho_n=rob, mode=mode)), cex.main=cex_paper)
 grid(col="gray", lwd=1)
-legend("bottomright", pch=c(1,NA), col=c(c1, c2), legend=c("Ratio", expression(kappa[1])), lty=c(1,2), cex=cex_paper)
+legend("bottomright", pch=c(pchstyle,NA), col=colors, legend=c("Ratio", expression(kappa[1])), lty=c(1,2), cex=cex_paper)
 dev.off()
-#y_min = min(intrinsic[,1], min(extrinsic[,1]))
-#y_max = max(intrinsic[,1], extrinsic[,1]+t[,4], t[,4])
-pdf(paste(args[7], "_activity.pdf", sep=""))
+
+print(" ++ Plotting activity")
+pdf(paste(graph_name, "_activity.pdf", sep=""))
+min_y = min(min(sim_act_y), min(data$real_act_y))
+max_y = max(max(sim_act_y), max(data$real_act_y))
 par(mar=c(5,5,4,5)+.1)
-plot(t[,3], t[,4], type="l", pch=4,xlab=expression(tau ~ " (in months)"), 
-     ylab=expression("Simulated Activity over " ~ a[c]), lty=2,
-     col=c2, cex=cex_size, cex.axis=cex_paper, cex.lab=cex_paper)#, ylim=c(y_min, y_max))
-title(substitute(atop("Activity over " ~ tau ~ " (in months)", 
+plot(sim_act_x[,1], sim_act_y[,1], type="l", pch=4, xlab=xlabel, ylab="Activity", lty=1, col=colors[1], cex=cex_size, cex.axis=cex_paper, cex.lab=cex_paper, ylim=c(min_y, max_y))
+lines(data$real_act_x, data$real_act_y, type=linetype, lty=1, pch=1, col=colors[2])
+title(substitute(atop("Activity over " ~ tau ~ " (in " * mode * ")", 
                       Delta ~ tau == ~ dt ~ ", " ~ mu == ~ dp ~ ", " ~ a[c] == ~ ac),
-                 list(dt = dtau, dp = dpsi, ac = ac)), cex.main=cex_paper)
+                 list(dt = dtau, dp = mu, ac = ac, mode=mode)), cex.main=cex_paper)
 grid(col="gray", lwd=1)
-par(new=TRUE)
-plot(t[,5], t[,6], type="o", lty=1, pch=2,xaxt="n",yaxt="n",xlab="",ylab="", 
-     col=c1, cex=cex_size, cex.axis=cex_paper, cex.lab=cex_paper)
-axis(4, cex.axis=cex_paper)
-mtext("Real Activity",side=4,line=3, cex=cex_paper)
-legend("topleft", pch=c(NA,2), col=c(c2, c1), legend=c("Simulated Activity", "Observed Activity"), lty=c(2,1), cex=cex_paper)
+#par(new=TRUE)
+#plot(data$real_act_x, data$real_act_y, type="o", lty=1, pch=2,xaxt="n",yaxt="n",xlab="",ylab="", 
+#     col="#858585", cex=cex_size, cex.axis=cex_paper, cex.lab=cex_paper)
+#axis(4, cex.axis=cex_paper)
+#mtext("Real Activity",side=4,line=3, cex=cex_paper)
+legend("topright", pch=c(NA,pchstyle), col=colors, legend=c("Simulated Activity", "Observed Activity"), lty=c(1,1), cex=cex_paper)
+dev.off()
+
+
+print(" ++ Plotting Error of Simulation")
+sim_act_list <- list()
+i = 1
+while(i <= length(data$real_act_x)) {
+  #print(i)
+  closest_tau = which(abs(sim_act_x - data$real_act_x[i])==min(abs(sim_act_x - data$real_act_x[i])))
+  #print(closest_tau)
+  sim_act_list[length(sim_act_list) + 1] <- sim_act_y[closest_tau,]
+  i = i + 1
+}
+#print(length(data$real_act_y))
+#print(length(sim_act_list))
+errors <- as.numeric(sim_act_list) - data$real_act_y
+errors_x <- seq(0,length(errors) - 1,1)
+#print(errors)
+rmse <- sqrt(mean(errors^2))
+#print(rmse)
+pdf(paste(graph_name, "_error.pdf", sep=""))
+plot(errors_x, errors, type="l", xlab=xlabel, ylab="Activity", lty=1, col=colors[1], cex.axis=cex_paper, cex.lab=cex_paper)
+title(substitute(atop("Error of Simulation over " ~ tau ~ " (in " * mode * ")",
+                      "(RMSE = " ~ rmse ~ ")"), list(rmse=rmse, mode=mode)), cex.main=cex_paper)
+grid(col="gray", lwd=1)
 dev.off()
