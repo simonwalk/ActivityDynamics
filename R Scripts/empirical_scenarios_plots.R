a938fb9ff338f7c4913eb6d6c1d083a169edc373
Philipp Koncar <p.koncar@student.tugraz.at>
2015-04-30 16:47:32 +0200

    scenarios: #11 (plot improvements)

--- R Scripts/empirical_scenarios_plots.R
+++ R Scripts/empirical_scenarios_plots.R
@@ -25,7 +25,7 @@ print(" ++ Plotting activity")
 if (format == "pdf") pdf(file_name) else png(file_name)
 min_y = min(min(weights$sim_act), min(data$real_act_y))
 max_y = max(max(weights$sim_act), max(data$real_act_y))
-par(mar=c(5,5,4,5)+.1)
+par(mar=c(8,5,4,5)+.1, xpd=T)
 plot(weights$taus, weights$sim_act, type="l", xlab=expression(tau ~ " (in months)"), ylab="Activity", lty=1, col=colors[1], cex=cex_size, cex.axis=cex_paper, cex.lab=cex_paper, ylim=c(min_y, max_y))
 lines(data$real_act_x, data$real_act_y, type="l", lty=1, pch=1, col=colors[2])
 
@@ -34,17 +34,18 @@ while(i <= length(weights)) {
   lines(weights$taus, weights[,i], type="l", lty=1, col=colors[i])
   i = i +1
 }
+legend("bottom", inset=c(0, -0.3), pch=c(NA,NA), col=colors, legend=legend_text, lty=c(1,1), cex=0.7, horiz=T)
 
-abline(v=marker, lty=2, col=colors[1])
-
+par(mar=c(8,5,4,5)+.1, xpd=F)
 title(substitute(atop(scenario * " for " * graph_name, 
                       "with " * rand_itas * " Random Iterations"),
                  list(scenario = scenario, graph_name = graph_name, rand_itas = rand_itas)), cex.main=cex_paper)
+abline(v=marker, lty=2, col=colors[1])
 grid(col="gray", lwd=1)
 #par(new=TRUE)
 #plot(data$real_act_x, data$real_act_y, type="o", lty=1, pch=2,xaxt="n",yaxt="n",xlab="",ylab="", 
 #     col="#858585", cex=cex_size, cex.axis=cex_paper, cex.lab=cex_paper)
 #axis(4, cex.axis=cex_paper)
 #mtext("Real Activity",side=4,line=3, cex=cex_paper)
-legend("topright", pch=c(NA,NA), col=colors, legend=legend_text, lty=c(1,1), cex=cex_paper)
+
 dev.off()
--- calc_scenarios_empiric.py
+++ calc_scenarios_empiric.py
@@ -10,7 +10,7 @@ matplotlib.use('Agg')
 from lib.generator import *
 from lib.scenario_network import ScenarioNetwork
 
-plot_only = False
+plot_only = True
 
 deltatau = 0.001
 store_itas = 10
